b690a777dcbbaeb11eb5c87d81d2b162db56e070
Bissam Iftikhar <bissamiftikhar@gmail.com>
2025-07-27 00:56:51 +0500

    frst commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"message": "API is working!"}
+
+# Todo list storage
+todos = []
+
+@app.post("/todos")
+def add_todo(task: str):
+    todos.append(task)
+    return {"message": "Todo added!"}
+
+@app.get("/todos")
+def get_todos():
+    return {"todos": todos}
