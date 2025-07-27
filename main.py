138db5797f249a5e637875b5ad4b14f58c7e4a5f
Bissam Iftikhar <bissamiftikhar@gmail.com>
2025-07-28 04:03:33 +0500

    deploy ho gayaIA   sss dd

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ todos = []
 @app.post("/todos")
 def add_todo(task: str):
     todos.append(task)
-    return {"message": "Todo ci.cd ya Allah ,done ia, IA no docker hsameetadded!"}
+    return {"message": "Todo ci.cd ya Allah  .. ,done ia, IA no rahh docker hsameetadded!"}
 
 @app.get("/todos")
 def get_todos():
