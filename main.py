17d4b327ff0203edc605cb976dd67b4d9d67a48d
Bissam Iftikhar <bissamiftikhar@gmail.com>
2025-07-28 02:49:32 +0500

    retry deploy after fixing secwwrets

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ todos = []
 @app.post("/todos")
 def add_todo(task: str):
     todos.append(task)
-    return {"message": "Todo ci.cd ya Allah hsameetadded!"}
+    return {"message": "Todo ci.cd ya Allah , IA hsameetadded!"}
 
 @app.get("/todos")
 def get_todos():
